85e86e38c996b34222a65f29e412c8a5ca34c2b0
HARSH <harsh.pyjs@gmail.com>
2025-09-29 19:09:50 +0530

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,20 +1,27 @@
-module.exports = {
+import User from '../models/User.js';
+
+export default {
   name: 'messageCreate',
   async execute(message, client) {
     if (message.author.bot) return;
 
-    // Example: Leveling system
-    const User = require('../models/User');
-    let user = await User.findOne({ userId: message.author.id });
-    if (!user) {
-      user = new User({ userId: message.author.id, xp: 0, level: 1 });
+    // AutoMod: delete Discord invites
+    if (message.content.includes('discord.gg')) {
+      await message.delete();
+      message.channel.send(`${message.author}, invite links are not allowed!`);
     }
+
+    // Leveling system
+    let user = await User.findOne({ userId: message.author.id });
+    if (!user) user = new User({ userId: message.author.id, xp: 0, level: 1 });
+
     user.xp += Math.floor(Math.random() * 10) + 1;
     if (user.xp >= user.level * 100) {
       user.level += 1;
       user.xp = 0;
       message.channel.send(`${message.author}, you leveled up to level ${user.level}!`);
     }
+
     await user.save();
-  },
+  }
 };
