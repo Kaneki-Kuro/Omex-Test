c85e7acc421791a1e7d60879760aa222ee7e7e62
Kaneki-Kuro <mukesh.79.nbh.coomay@gmail.com>
2025-10-01 19:11:13 +0530

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -12,13 +12,13 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
-// Register for a single guild (faster for testing)
-const GUILD_ID = 'YOUR_TEST_GUILD_ID';
-
 try {
-  console.log('Started refreshing application (/) commands.');
-  await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, GUILD_ID), { body: commands });
-  console.log('Successfully reloaded application (/) commands.');
+  console.log('Started refreshing global application (/) commands.');
+  await rest.put(
+    Routes.applicationCommands(process.env.CLIENT_ID), // Global commands
+    { body: commands }
+  );
+  console.log('Successfully reloaded global application (/) commands.');
 } catch (error) {
   console.error(error);
 }
