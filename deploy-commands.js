8d32b86476787f3e04b7493895f9b9adedc58d14
Kaneki-Kuro <mukesh.79.nbh.coomay@gmail.com>
2025-10-02 18:22:39 +0530

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,24 +1,38 @@
+// deploy-commands.js
 import 'dotenv/config';
 import { REST, Routes } from 'discord.js';
 import fs from 'fs';
 
 const commands = [];
+
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-  const { default: command } = await import(`./commands/${file}`);
-  commands.push(command.data.toJSON());
+  try {
+    const { default: command } = await import(`./commands/${file}`);
+    if ('data' in command && 'execute' in command) {
+      commands.push(command.data.toJSON());
+    } else {
+      console.warn(`[WARNING] The command at ${file} is missing "data" or "execute".`);
+    }
+  } catch (err) {
+    console.error(`Error loading command ${file}:`, err);
+  }
 }
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
-try {
-  console.log('Started refreshing global application (/) commands.');
-  await rest.put(
-    Routes.applicationCommands(process.env.CLIENT_ID), // Global commands
-    { body: commands }
-  );
-  console.log('Successfully reloaded global application (/) commands.');
-} catch (error) {
-  console.error(error);
-}
+(async () => {
+  try {
+    console.log(`Started refreshing ${commands.length} global application (/) commands.`);
+
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands },
+    );
+
+    console.log(`Successfully reloaded ${commands.length} global application (/) commands.`);
+  } catch (error) {
+    console.error(error);
+  }
+})();
