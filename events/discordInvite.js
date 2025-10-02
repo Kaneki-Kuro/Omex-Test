167bdba07096d868287d5cead03fb9d91a490b5e
Kaneki-Kuro <mukesh.79.nbh.coomay@gmail.com>
2025-10-02 13:09:43 +0530

    Update discordInvite.js

--- events/discordInvite.js
+++ events/discordInvite.js
@@ -17,19 +17,3 @@ export default {
         return; // ⬅️ stop here (NO leveling check for invite messages)
       }
     }
-
-    // ===== Leveling System =====
-    let user = await User.findOne({ userId: message.author.id });
-    if (!user) user = new User({ userId: message.author.id, xp: 0, level: 1 });
-
-    user.xp += Math.floor(Math.random() * 10) + 1;
-
-    if (user.xp >= user.level * 100) {
-      user.level += 1;
-      user.xp = 0;
-      await message.channel.send(`${message.author}, you leveled up to level ${user.level}!`);
-    }
-
-    await user.save();
-  }
-};
