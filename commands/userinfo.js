83ed9528abd29baaf6df41fbd4fa1d0dc4bc5f93
Kaneki-Kuro <mukesh.79.nbh.coomay@gmail.com>
2025-10-04 18:00:52 +0530

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,42 +1,23 @@
-import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
+import { EmbedBuilder } from "discord.js";
 
-export const data = new SlashCommandBuilder()
-  .setName("userinfo")
-  .setDescription("Shows info about a user")
-  .addUserOption(option =>
-    option.setName("target")
-      .setDescription("The user to get info about")
-      .setRequired(false)
-  );
+export const data = {
+  name: "userinfo"
+};
 
 export async function execute(interactionOrMessage, args) {
   try {
     const isSlash = typeof interactionOrMessage.isChatInputCommand === "function";
 
+    // Determine user
     let user;
     if (isSlash) {
       user = interactionOrMessage.options.getUser("target") || interactionOrMessage.user;
     } else {
       const message = interactionOrMessage;
-      user =
-        message.mentions.users.first() ||
-        (args?.[0] ? await message.client.users.fetch(args[0]).catch(() => null) : null) ||
-        message.author;
+      user = message.mentions.users.first() || message.author;
     }
 
-    if (!user) {
-      if (isSlash) {
-        return await interactionOrMessage.reply({
-          content: "❌ Couldn't find that user!",
-          ephemeral: true
-        });
-      } else {
-        return await interactionOrMessage.channel.send("❌ Couldn't find that user!");
-      }
-    }
-
-    const member = await interactionOrMessage.guild.members.fetch(user.id).catch(() => null);
-
+    // Create embed
     const embed = new EmbedBuilder()
       .setTitle(`${user.tag}`)
       .setThumbnail(user.displayAvatarURL({ dynamic: true }))
@@ -44,11 +25,6 @@ export async function execute(interactionOrMessage, args) {
         { name: "ID", value: user.id, inline: true },
         { name: "Username", value: user.username, inline: true },
         { name: "Discriminator", value: `#${user.discriminator}`, inline: true },
-        {
-          name: "Joined Server",
-          value: member ? `<t:${Math.floor(member.joinedTimestamp / 1000)}:R>` : "N/A",
-          inline: true
-        },
         {
           name: "Account Created",
           value: `<t:${Math.floor(user.createdTimestamp / 1000)}:R>`,
@@ -72,10 +48,7 @@ export async function execute(interactionOrMessage, args) {
 
   } catch (err) {
     console.error("❌ Error in userinfo:", err);
-    // safe error message
-    if (interactionOrMessage.reply && !interactionOrMessage.replied && !interactionOrMessage.deferred) {
-      await interactionOrMessage.reply({ content: "❌ Error executing command!", ephemeral: true });
-    } else if (interactionOrMessage.channel) {
+    if (interactionOrMessage.channel) {
       await interactionOrMessage.channel.send("❌ Error executing command!");
     }
   }
