b5c7c957ac82f836e1e263c3adbed1671cd285c1
Kaneki-Kuro <mukesh.79.nbh.coomay@gmail.com>
2025-10-04 17:53:24 +0530

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,41 +1,69 @@
 import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
 
-export default {
-  data: new SlashCommandBuilder()
-    .setName("userinfo")
-    .setDescription("Shows information about a user.")
-    .addUserOption(option =>
-      option.setName("target").setDescription("The user to get info about")
-    ),
-  async execute(interaction) {
-    const user = interaction.options.getUser("target") || interaction.user;
-    const member = await interaction.guild.members.fetch(user.id);
+export const data = new SlashCommandBuilder()
+  .setName("userinfo")
+  .setDescription("Shows info about a user")
+  .addUserOption(option =>
+    option.setName("target")
+      .setDescription("The user to get info about")
+      .setRequired(false)
+  );
 
-    const embed = new EmbedBuilder()
-      .setTitle(`${user.tag}`)
-      .setThumbnail(user.displayAvatarURL({ dynamic: true }))
-      .addFields(
-        { name: "ID", value: user.id, inline: true },
-        { name: "Username", value: user.username, inline: true },
-        { name: "Discriminator", value: `#${user.discriminator}`, inline: true },
-        {
-          name: "Joined Server",
-          value: `<t:${Math.floor(member.joinedTimestamp / 1000)}:R>`,
-          inline: true
-        },
-        {
-          name: "Account Created",
-          value: `<t:${Math.floor(user.createdTimestamp / 1000)}:R>`,
-          inline: true
-        }
-      )
-      .setColor("Purple")
-      .setFooter({
-        text: `Requested by ${interaction.user.tag}`,
-        iconURL: interaction.user.displayAvatarURL()
-      })
-      .setTimestamp();
+export async function execute(interactionOrMessage, args) {
+  // Detect if it's slash or prefix
+  const isSlash = !!interactionOrMessage.isChatInputCommand;
 
-    await interaction.reply({ embeds: [embed] });
+  let user;
+  if (isSlash) {
+    // Slash command case
+    user = interactionOrMessage.options.getUser("target") || interactionOrMessage.user;
+  } else {
+    // Prefix case
+    const message = interactionOrMessage;
+    user =
+      message.mentions.users.first() ||
+      (args?.[0] ? await message.client.users.fetch(args[0]).catch(() => null) : null) ||
+      message.author;
   }
-};
+
+  if (!user) {
+    if (isSlash) {
+      return interactionOrMessage.reply({ content: "❌ Couldn't find that user!", ephemeral: true });
+    } else {
+      return interactionOrMessage.channel.send("❌ Couldn't find that user!");
+    }
+  }
+
+  const member = await interactionOrMessage.guild.members.fetch(user.id);
+
+  const embed = new EmbedBuilder()
+    .setTitle(`${user.tag}`)
+    .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+    .addFields(
+      { name: "ID", value: user.id, inline: true },
+      { name: "Username", value: user.username, inline: true },
+      { name: "Discriminator", value: `#${user.discriminator}`, inline: true },
+      {
+        name: "Joined Server",
+        value: `<t:${Math.floor(member.joinedTimestamp / 1000)}:R>`,
+        inline: true
+      },
+      {
+        name: "Account Created",
+        value: `<t:${Math.floor(user.createdTimestamp / 1000)}:R>`,
+        inline: true
+      }
+    )
+    .setColor("Purple")
+    .setFooter({
+      text: `Requested by ${isSlash ? interactionOrMessage.user.tag : interactionOrMessage.author.tag}`,
+      iconURL: (isSlash ? interactionOrMessage.user : interactionOrMessage.author).displayAvatarURL()
+    })
+    .setTimestamp();
+
+  if (isSlash) {
+    await interactionOrMessage.reply({ embeds: [embed] });
+  } else {
+    await interactionOrMessage.channel.send({ embeds: [embed] });
+  }
+}
