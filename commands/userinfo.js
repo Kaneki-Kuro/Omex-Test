f6af60078abc2cfa4557dde635c17a3f2932eaea
Kaneki-Kuro <mukesh.79.nbh.coomay@gmail.com>
2025-10-04 17:52:05 +0530

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,16 +1,13 @@
 import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
 
-export const data = new SlashCommandBuilder()
-  .setName("userinfo")
-  .setDescription("Shows detailed user info")
-  .addUserOption(option =>
-    option.setName("target")
-      .setDescription("Select a user")
-      .setRequired(false)
-  );
-
-export async function execute(interaction) {
-  try {
+export default {
+  data: new SlashCommandBuilder()
+    .setName("userinfo")
+    .setDescription("Shows information about a user.")
+    .addUserOption(option =>
+      option.setName("target").setDescription("The user to get info about")
+    ),
+  async execute(interaction) {
     const user = interaction.options.getUser("target") || interaction.user;
     const member = await interaction.guild.members.fetch(user.id);
 
@@ -21,24 +18,24 @@ export async function execute(interaction) {
         { name: "ID", value: user.id, inline: true },
         { name: "Username", value: user.username, inline: true },
         { name: "Discriminator", value: `#${user.discriminator}`, inline: true },
-        { name: "Joined Server", value: `<t:${Math.floor(member.joinedTimestamp / 1000)}:R>`, inline: true },
-        { name: "Account Created", value: `<t:${Math.floor(user.createdTimestamp / 1000)}:R>`, inline: true }
+        {
+          name: "Joined Server",
+          value: `<t:${Math.floor(member.joinedTimestamp / 1000)}:R>`,
+          inline: true
+        },
+        {
+          name: "Account Created",
+          value: `<t:${Math.floor(user.createdTimestamp / 1000)}:R>`,
+          inline: true
+        }
       )
       .setColor("Purple")
-      .setFooter({ text: `Requested by ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL() })
+      .setFooter({
+        text: `Requested by ${interaction.user.tag}`,
+        iconURL: interaction.user.displayAvatarURL()
+      })
       .setTimestamp();
 
-    if (interaction.deferred || interaction.replied) {
-      await interaction.editReply({ embeds: [embed] });
-    } else {
-      await interaction.reply({ embeds: [embed] });
-    }
-  } catch (err) {
-    console.error(err);
-    if (interaction.deferred || interaction.replied) {
-      await interaction.editReply({ content: "❌ Error executing command!", ephemeral: true });
-    } else {
-      await interaction.reply({ content: "❌ Error executing command!", ephemeral: true });
-    }
+    await interaction.reply({ embeds: [embed] });
   }
-}
+};
